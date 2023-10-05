ee29fc1bd1abaf9984eb44117f5936c9f1b2c90c
jeanricken12 <94722277+jeanricken12@users.noreply.github.com>
2023-10-05 00:36:19 -0300

    Finish

--- Assets/Pixel Adventure 1/Assets/Scripts/Player.cs
+++ Assets/Pixel Adventure 1/Assets/Scripts/Player.cs
@@ -1,27 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
+    public int maxHealth = 3; // Vida máxima do jogador
 
-    public bool isJumping;
-    public bool dubleJump;
-
+    private bool isJumping;
+    private bool doubleJump;
     private Rigidbody2D rig;
     private Animator anim;
-    
+    private int currentHealth;
+
     void Start()
     {
-        rig =  GetComponent<Rigidbody2D>();
+        rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        currentHealth = maxHealth;
+        UpdateHealthUI();
     }
 
-    
     void Update()
     {
+        if (currentHealth <= 0)
+        {
+            // Se a vida do jogador for menor ou igual a zero, encerre o jogo.
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
+
         Move();
         Jump();
     }
@@ -33,45 +43,45 @@ public class Player : MonoBehaviour
 
         if (Input.GetAxis("Horizontal") > 0f)
         {
-           anim.SetBool("walk", true);
+            anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
-
-        if(Input.GetAxis("Horizontal") < 0f)
+        else if (Input.GetAxis("Horizontal") < 0f)
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
-
         }
-
-        if (Input.GetAxis("Horizontal") == 0f)
+        else
         {
             anim.SetBool("walk", false);
         }
-
     }
 
     void Jump()
     {
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
-            if(!isJumping)
+            if (!isJumping)
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                dubleJump = true;
+                doubleJump = true;
                 anim.SetBool("jump", true);
             }
             else
             {
-                if (dubleJump)
+                if (doubleJump)
                 {
                     rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                    dubleJump = false;
+                    doubleJump = false;
                 }
-
             }
-            
         }
+    }
+
+    void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        UpdateHealthUI();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -81,23 +91,35 @@ public class Player : MonoBehaviour
             isJumping = false;
             anim.SetBool("jump", false);
         }
-        if (collision.gameObject.tag == "Spike")
+        if (collision.gameObject.CompareTag("Spike") || collision.gameObject.CompareTag("Saw"))
         {
-            GameController.instance.ShowGameOver();
-            Destroy(gameObject);
-        }
-        if (collision.gameObject.tag == "Saw")
-        {
-            GameController.instance.ShowGameOver();
-            Destroy(gameObject);
+            TakeDamage(1); // 1 é o valor de dano ao jogador
         }
     }
 
-    void OnCollisionExit2D(Collision2D collision)
+    void UpdateHealthUI()
     {
-        if (collision.gameObject.layer == 8)
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        string healthBar = "Health: ";
+        for (int i = 0; i < maxHealth; i++)
         {
-            isJumping = true;
+            if (i < currentHealth)
+            {
+                healthBar += "#"; // Adiciona "#" para representar a vida atual
+            }
+            else
+            {
+                healthBar += "-"; // Adiciona "-" para representar a vida perdida
+            }
+        }
+        Debug.Log(healthBar); // Imprime a barrinha de vida no console
+
+        // Encontra o objeto de texto automaticamente se ele não estiver atribuído
+        Text healthTextComponent = FindObjectOfType<Text>();
+        if (healthTextComponent != null)
+        {
+            healthTextComponent.text = "Vidas: " + currentHealth.ToString();
         }
     }
 }
